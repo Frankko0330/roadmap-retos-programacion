2cdcadca100cfa500e3a1efe8fa24b806c71c225
Frankko0330 <frankko0330@gmail.com>
2026-04-16 10:09:39 -0400

    Solución reto #00 Python

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Frankko0330.py
@@ -0,0 +1,77 @@
+# Ejercicio 00. De Frankko0330
+
+# 1 URL del lenjuaje de programacion.
+
+# https://python.org/
+
+# 2 Lenguaje elejido
+
+# bien, ya he elegido un lenguaje, "Python", bueno es obvio jaja
+# ya que cree el archivo en esta capeta.
+# -pa el que lea, no me hagas caso, ando modo divague 
+
+# 3 comentarios que encontre para este lenguaje:
+
+"""
+creo va asi?, si se marca en azul cuenta como comentario?
+osea, se supone se llama comentario en varias lienas, pero realmente
+no altera el codigo asi?
+"""
+
+'''
+esto tambien es un cometario igalmente tonces, me imagino
+'''
+
+# Cierto, casi olvido que este tambien es uno :v
+
+# 4 crea una variable, y una constante (si el lenguaje lo sopora)
+
+huevo = "tengo huevos para desayunar :)"
+huevo = "resulta que tengo 2 y neserios son 3 :("
+
+variable_2 = "30"
+variable_2 = "33"
+
+#
+
+CONSTANTE = "AUN NO ENTIENDO PARA QUE SIRVE SI SE SUPONE NO EXISTE PARA ESTE LENGUAJE"
+
+# DUDA: Es nesesario usar el "" para crear un valor de variable?
+
+
+# 5 Crea variables representando todos los tipos de datos primitivos
+#-------------------------------------------------------------------------------------------
+variable_int = 8 # int, integers, entero. Dato que sirve mara representar numeros enteros 
+                 # peroo... entonces si le pongo el "" entonces me lo reconoce como palabra
+                 # y no como numero?
+print(type(variable_int))
+#-------------------------------------------------------------------------------------------
+variable_float = 0,8 # lo mismo pero con decimales
+variable_float = 8
+variable_float = 8,80
+print(type(variable_float))
+#------------------------------------------------------------------------------------------
+variable_bool = True   #este tipo de variable solo tiene dos tipos "los mostrados"
+variable_bool = False
+print(type(variable_bool))
+#-------------------------------------------------------------------------------------------
+variable_string = "linea de texto"
+variable_string = "linea de texto modificada"
+variable_str = 'asi tambien'
+variable_str = 'asi tambie fue'
+print(type(variable_str))
+#-------------------------------------------------------------------------------------------
+"""
+se que el print para que ejecute, imprima una palabra o eso es lo que entiendo pero el 
+prin(type()) para que es exactamente el type, veo que tria en la terminal 
+
+"""
+# 6  Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+
+print("Hola Mundo")
+
+#tipo de datos de cada uno
+
+
+
+print(type(variable_str))
